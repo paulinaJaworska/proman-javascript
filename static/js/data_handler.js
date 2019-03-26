77bc71e8d6b9e846b3e7865932192a4b17895c96
Paulina Jaworska <paulinajaworska9@gmail.com>
2019-03-26 01:50:06 +0100

    getBoards

--- static/js/data_handler.js
+++ static/js/data_handler.js
@@ -7,39 +7,49 @@ let dataHandler = {
     keyInLocalStorage: 'proman-data', // the string that you use as a key in localStorage to save your application data
     _data: {}, // it contains the boards and their cards and statuses. It is not called from outside.
     _loadData: function () {
-        this._data = JSON.parse(localStorage.getItem("proman-data"));
+        this._data = JSON.parse(localStorage.getItem('proman-data'));
     },
     _saveData: function () {
         // it is not called from outside
         // saves the data from this._data to local storage
-        localStorage.setItem("proman-data", JSON.stringify(this._data));
+        localStorage.setItem('proman-data', JSON.stringify(this._data));
     },
     init: function () {
         this._loadData();
     },
     getBoards: function (callback) {
         // the boards are retrieved and then the callback function is called with the boards
+        if (typeof callback !== 'function') throw new Error('Invalid callback handler');
+        boards = this._data['boards'];
+        callback(boards);
     },
     getBoard: function (boardId, callback) {
         // the board is retrieved and then the callback function is called with the board
+        if (typeof callback !== 'function') throw new Error('Invalid callback handler');
     },
     getStatuses: function (callback) {
         // the statuses are retrieved and then the callback function is called with the statuses
+        if (typeof callback !== 'function') throw new Error('Invalid callback handler');
     },
     getStatus: function (statusId, callback) {
         // the status is retrieved and then the callback function is called with the status
+        if (typeof callback !== 'function') throw new Error('Invalid callback handler');
     },
     getCardsByBoardId: function (boardId, callback) {
         // the cards are retrieved and then the callback function is called with the cards
+        if (typeof callback !== 'function') throw new Error('Invalid callback handler');
     },
     getCard: function (cardId, callback) {
         // the card is retrieved and then the callback function is called with the card
+        if (typeof callback !== 'function') throw new Error('Invalid callback handler');
     },
     createNewBoard: function (boardTitle, callback) {
         // creates new board, saves it and calls the callback function with its data
+        if (typeof callback !== 'function') throw new Error('Invalid callback handler');
     },
     createNewCard: function (cardTitle, boardId, statusId, callback) {
         // creates new card, saves it and calls the callback function with its data
+        if (typeof callback !== 'function') throw new Error('Invalid callback handler');
     }
     // here comes more features
 };
